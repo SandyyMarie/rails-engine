c0e25ebea2def4a5c84bef529c663aa2a893094d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-21 01:36:48 -0600

    feat: passing all tests including issues with postman, tabbed out relationships within serializers - all items also passing

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,7 +4,10 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def show
-    merchant = Merchant.find(params[:id])
-    render json: MerchantSerializer.new(merchant)
+    if Merchant.exists?(params[:id])
+      render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    else
+      render status: 404
+    end
   end
 end
