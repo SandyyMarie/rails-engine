6d471228501271de64007bd15d8b614edf259702
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-20 21:26:21 -0600

    refactor: updated factory bot with item factory

--- spec/factories/merchant.rb
+++ spec/factories/merchant.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :merchant, class: Merchant do
-    name { Faker::Name.name }
+  factory :item, class: Item do
+    name        {Faker::Commerce.product_name}
+    description {Faker::Marketing.buzzwords}
+    unit_price  {Faker::Number.within(range: 500..2000)}
+    association :merchant, factory: :merchant
   end
 end
