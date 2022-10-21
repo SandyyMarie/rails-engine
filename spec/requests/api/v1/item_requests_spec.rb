89d7a0583108872290e4ce7cc54576c66caab1ea
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-21 04:36:24 -0600

    test: added test for item #update

--- spec/requests/api/v1/item_requests_spec.rb
+++ spec/requests/api/v1/item_requests_spec.rb
@@ -67,4 +67,19 @@ describe "Items API" do
     expect(Item.count).to eq(0)
     expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Snow Shovel" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({book: items_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Snow Shovel")
+  end
 end
